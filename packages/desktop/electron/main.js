6d5c155d685a26a45a87019b4563375fcfa54e6f
Nicole O'Brien <nicole.obrien@iota.org>
2021-12-20 01:06:10 +0000

    fix: potential windows crash fix

--- packages/desktop/electron/main.js
+++ packages/desktop/electron/main.js
@@ -225,6 +225,14 @@ function createWindow() {
         windows.main = null
     })
 
+    windows.main.webContents.on('render-process-gone', function (event, detailed) {
+        if (detailed.reason == 'crashed') {
+            // relaunch app
+            app.relaunch({ args: process.argv.slice(1).concat(['--relaunch']) })
+            app.exit(0)
+        }
+    })
+
     /**
      * Handle permissions requests
      */
