94740f1b2c479048654ea1f1350bd1376d826fc5
Nicole O'Brien <nicole.obrien@iota.org>
2022-09-07 23:28:25 +0100

    chore: set onboarding features for ledger build chore: set coin type to 1 for testing

--- packages/shared/lib/core/network/constants/coin-type.constant.ts
+++ packages/shared/lib/core/network/constants/coin-type.constant.ts
@@ -2,5 +2,5 @@ import { NetworkProtocol } from '../enums'
 
 export const COIN_TYPE: Readonly<{ [key in NetworkProtocol]?: number }> = {
     [NetworkProtocol.IOTA]: 4218,
-    [NetworkProtocol.Shimmer]: 4219,
+    [NetworkProtocol.Shimmer]: 1,
 }
